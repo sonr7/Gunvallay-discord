e102801ca5c958df344ea5290ced4113f2ff5927
sonr7 <77051974+sonr7@users.noreply.github.com>
2021-07-11 15:04:48 +0900

    Update gunvallaybot.py

--- gunvallaybot.py
+++ gunvallaybot.py
@@ -110,7 +110,7 @@ async def on_message(message):
         root = f'√{root1}, {root2}'
         await message.channel.send(root)
     if '!d bump' in message.content:
-        time.sleep(5)
+        time.sleep(1)
         await message.channel.send('bumpの時間！bumpがんばれ👍')
    
         
