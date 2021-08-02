8d98e1fcb846980b4e5d8364dbe9dff67ec6a9f5
sonr7 <77051974+sonr7@users.noreply.github.com>
2021-08-02 21:04:55 +0900

    Update gunvallaybot.py

--- gunvallaybot.py
+++ gunvallaybot.py
@@ -115,14 +115,14 @@ async def on_message(message):
         await message.channel.send(now)
     if '#help' in message.content:
         embed = discord.Embed(title = "がんばれ君が助けに来た！")
-        embed.add_field(name = "応答", value = "たまに言葉で反応するときがあるよ！（「。」を使えば黙らせられるよー）", inline = false)
-        embed.add_field(name = "#p x y", value = "足し算できるよ！3個以上の数値もできるよ！（この場合はx+yになるよー）", inline = false)
-        embed.add_field(name = "#m x y", value = "引き算できるよ！3個以上の数値もできるよ！（この場合はx-yになるよー）", inline = false)
-        embed.add_field(name = "#t x y", value = "掛け算できるよ！3個以上の数値もできるよ！（この場合はx×yになるよー）", inline = false)
-        embed.add_field(name = "#d x y", value = "割り算できるよ！3個以上の数値もできるよ！（この場合はx÷yになるよー）", inline = false)
-        embed.add_field(name = "#o x y", value = "割り算あまりできるよ！", inline = false)
-        embed.add_field(name = "#s x y", value = "累乗できるよ！（この場合はxのy乗になるよー）", inline = false)
-        embed.add_field(name = "#r x", value = "ルートの値求めてくれるよ！", inline = false)
+        embed.add_field(name = "応答", value = "たまに言葉で反応するときがあるよ！（「。」を使えば黙らせられるよー）", inline = False)
+        embed.add_field(name = "#p x y", value = "足し算できるよ！3個以上の数値もできるよ！（この場合はx+yになるよー）", inline = False)
+        embed.add_field(name = "#m x y", value = "引き算できるよ！3個以上の数値もできるよ！（この場合はx-yになるよー）", inline = False)
+        embed.add_field(name = "#t x y", value = "掛け算できるよ！3個以上の数値もできるよ！（この場合はx×yになるよー）", inline = False)
+        embed.add_field(name = "#d x y", value = "割り算できるよ！3個以上の数値もできるよ！（この場合はx÷yになるよー）", inline = False)
+        embed.add_field(name = "#o x y", value = "割り算あまりできるよ！", inline = False)
+        embed.add_field(name = "#s x y", value = "累乗できるよ！（この場合はxのy乗になるよー）", inline = False)
+        embed.add_field(name = "#r x", value = "ルートの値求めてくれるよ！", inline = False)
         await message.channel.send(embed = embed)
 
             
