543a7d1dd0cb38877257891319cdbe3a42d9b2f2
sonr7 <77051974+sonr7@users.noreply.github.com>
2021-08-07 21:54:22 +0900

    Update gunvallaybot.py

--- gunvallaybot.py
+++ gunvallaybot.py
@@ -211,7 +211,7 @@ async def on_message(message):
         embed = discord.Embed()
         for pages in page_ach:
             pages_int = page_ach.index(pages)
-            pagespages_url = f'https://ja.wikipedia.org/wiki/{pages}'
+            pages_url = f'https://ja.wikipedia.org/wiki/{pages}'
             embed.add_field(name = pages, value = f'「{pages}」で再検索', inline = False)
         await message.channel.send(embed = embed)
             
