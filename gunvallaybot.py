ed19f0d906da7cc4285db53aa524d80ac83330e4
sonr7 <77051974+sonr7@users.noreply.github.com>
2021-08-07 21:16:14 +0900

    Update gunvallaybot.py

--- gunvallaybot.py
+++ gunvallaybot.py
@@ -197,9 +197,16 @@ async def on_message(message):
             page_int = page_search.index(page)
             page_url = f'https://ja.wikipedia.org/wiki/{page}'
             embed.add_field(name = page, value = f'{page}なら{page_int}', inline = False)
-            embed.remove_field(0)
         await message.channel.send(embed = embed)
-            
+        if message.content == '1' or message.content == '2' or message.content == '3' or message.content == '4' or message.content == '5' or message.content == '6' or message.content == '7' or message.content == '8' or message.content == '9' or message.content == '10':
+            page_urls = f'https://ja.wikipedia.org/wiki/{page_search[page_int]}'
+            embed2 = discord.Embed(title = page_search[page_int], url = page_urls)
+            page_naming = wikipedia.page(page_search[page_int])
+            page_summary = wikipedia.summary(page_search[page_int]
+            embed2.add_field(name = page_naming, value = page_summary, inline = False)
+            await message.channel.send(embed2 = embed2)
+                                             
+                                             
         
        
                    
