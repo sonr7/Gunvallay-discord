30a68ea935bd20a256144c243456e8d5fc1768ae
sonr7 <77051974+sonr7@users.noreply.github.com>
2021-08-30 13:13:02 +0900

    Update gunvallaybot.py

--- gunvallaybot.py
+++ gunvallaybot.py
@@ -107,7 +107,7 @@ async def loop():
     
 @client.event
 async def on_member_join(member):
-    await channel.send('よろしく！')
+    await send('よろしく！')
      
 @client.event
 async def on_message(message):
