9fdc2439fbf9c72568fc7361348a40d1325fa4e8
sonr7 <77051974+sonr7@users.noreply.github.com>
2021-09-08 08:41:15 +0900

    Update gunvallaybot.py

--- gunvallaybot.py
+++ gunvallaybot.py
@@ -114,6 +114,8 @@ async def on_member_join(member):
 @client.event
 async def on_message(message):
     print(message.author.name + "<" + message.content)
+    if message.author.bot:
+        return
     if '。' in message.content:
         return
     if 'いってき' in message.content:
