769796b36de5baa1e7c3785289c9cedfeb2cc313
sonr7 <77051974+sonr7@users.noreply.github.com>
2021-09-14 13:41:43 +0900

    Update gunvallaybot.py

--- gunvallaybot.py
+++ gunvallaybot.py
@@ -218,7 +218,7 @@ async def on_message(message):
         embed.add_field(name = "`#llt x y z`", value = "ルーレットできるよ！（この場合はx,y,z,のどれかが出るよ！", inline = False)
         embed.add_field(name = "`#ebr`", value = "鯖内のメンバー数、人数、BOT数がわかるよ！", inline = False)
         embed.add_field(name = "`#fjk`", value = "くぁwせdrftgyふじこlp", inline = False)
-        embed.add_field(name = "`#wiki`", value = "wikiで検索してくれるよ！", inline = False)
+        embed.add_field(name = "`#wiki:`", value = "wikiで検索してくれるよ！(コロン「:」を忘れないでね！)", inline = False)
         embed.add_field(name = "`#wach `", value = "wikiでxの検索候補を10個表示してくれるよ！", inline = False)
         embed.add_field(name = '`#ranks`', value = 'それぞれのみんはやのランクの人数を教えてくれるよ！', inline = False)
         embed.add_field(name = '`#zikan`', value = 'タイマーを使えるよ！', inline = False)
@@ -298,12 +298,12 @@ async def on_message(message):
         await reply(message)
     if '#zikan' in message.content:
         await zikan(message)
-    if '#wiki'in message.content:
-        re.sub('#wiki', '', message.content)
+    if '#wiki:'in message.content:
+        wiki0, wiki1 = message.content.split(':', 1)
         wikipedia.set_lang('ja')
         try:
-            page_title = wikipedia.page(message.content)
-            embed = discord.Embed(title = message.content, url = f'https://ja.wikipedia.org/wiki/{message.content}')
+            page_title = wikipedia.page(wiki1)
+            embed = discord.Embed(title = message.content, url = f'https://ja.wikipedia.org/wiki/{wiki1}')
             page_summary = wikipedia.summary(message.content)
             embed.add_field(name = page_title, value = page_summary, inline = False)
             await message.channel.send(embed = embed)
