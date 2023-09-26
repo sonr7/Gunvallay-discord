3f3ada477ca3d8f89acda316516163cf6e0348e1
sonr7 <77051974+sonr7@users.noreply.github.com>
2023-09-26 20:25:45 +0900

    Update gunvallaybot.py

--- gunvallaybot.py
+++ gunvallaybot.py
@@ -3,11 +3,11 @@ import os
 from datetime import datetime
 
 bot = discord.Bot(
-        intents = discord.Intents.all(), 
-        activity = discord.Game("JPSをプレイ中"),
+    intents=discord.Intents.all(),
+    activity=discord.Game("JPSをプレイ中"),
 )
 
-token=os.environ.get("token")
+token = os.environ.get("token")
 
 Notification_list = []
 
@@ -18,7 +18,7 @@ async def on_message(message: discord.Message):
     if message.content == 'hello':
         await message.reply("Hello!")
 
-@bot.command(name = "Register", discription = "告知メッセージの登録")
+@bot.command(name="Register", discription="告知メッセージの登録")
 async def register(ctx: discord.ApplicationContext, Content: str):
     Notification_list.append(Content)
     await ctx.respond("告知メッセージの登録完了")
@@ -27,13 +27,13 @@ async def register(ctx: discord.ApplicationContext, Content: str):
         if NOW == "103000":
             await ctx.respond(Notification_list.pop(0))
 
-@bot.command(name = "list", discription = "告知メッセージのリストを確認")
+@bot.command(name="list", discription="告知メッセージのリストを確認")
 async def list(ctx: discord.ApplicationContext):
     await ctx.respond(Notification_list)
 
-@bot.command(name = "delete", discription = "最後に登録された告知メッセージの削除")
+@bot.command(name="delete", discription="最後に登録された告知メッセージの削除")
 async def delete(ctx: discord.ApplicationContext):
     del Notification_list[-1]
     await ctx.respond(f'{int(len(Notification_list)) + 1}番目の告知メッセージを削除')
-                                               
+                                 
 bot.run(token)
